b277a9891d398f64b58792b9f8e7be79eeea1273
daljeet mathur <daljeet.mathur2002@gmail.com>
2025-06-03 12:36:16 +0530

    add marker and update flesh,css

--- public/js/app.js
+++ public/js/app.js
@@ -40,25 +40,3 @@ window.addEventListener('load', function () {
   }, 400);
 });
 
-
-
-// mapbox
-
-    mapboxgl.accessToken = map_token;
-    console.log(map_token)
-
-    // const map = new mapboxgl.Map({
-    //     container: 'map',
-    //    
-    //     projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-    //     zoom: 1,
-    //     center: [ 77.1025, 28.7041]
-    // });
-
-  mapboxgl.accessToken = map_token;
-  const map = new mapboxgl.Map({
-      style: 'mapbox://styles/mapbox/streets-v12',
-      container: 'map',
-      center: [-74.5, 40],
-      zoom: 9
-  });
--- public/js/map.js
+++ public/js/map.js
@@ -10,10 +10,23 @@
     //     center: [ 77.1025, 28.7041]
     // });
 
-  mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+    mapboxgl.accessToken = map_token;
   const map = new mapboxgl.Map({
       style: 'mapbox://styles/mapbox/streets-v12',
       container: 'map',
-      center: [-74.5, 40],
+      center:  listing.geometry.coordinates  ,
       zoom: 9
   });
+
+
+
+      // Create a default Marker and add it to the map.
+ const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat( listing.geometry.coordinates ) // Listing.geometry coordinates
+    .setPopup(new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+            `<h4>${listing.location}</h4 > <p>Exact Location will be provided after booking!</p>`
+
+        )
+    )
+    .addTo(map);
